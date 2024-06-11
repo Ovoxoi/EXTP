dec7797920aa1b4a21350d89afa5fa93bdd5ab8b
Ovoxoi <129445931+Ovoxoi@users.noreply.github.com>
2024-06-11 16:31:36 +0000

    creation de data

--- components/CategoryButton.tsx
+++ components/CategoryButton.tsx
@@ -1,12 +1,38 @@
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 import colors from "@/constants/colors";
+import destinationCategories from "@/data/categories";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const CategoryButton = () => {
   return (
     <View>
       <Text style={Styles.title}>Categories</Text>
-      <ScrollView></ScrollView>
+      <ScrollView
+        horizontal
+        contentContainerStyle={{
+          gap: 20,
+          paddingVertical: 10,
+          marginBottom: 10,
+        }}
+      >
+        {destinationCategories.map((item, index) => (
+          <TouchableOpacity onPress={() => {}} style={Styles.categoryBtn}>
+            <MaterialCommunityIcons
+              name={item.iconName as any}
+              size={20}
+              color={colors.black}
+            />
+            <Text>{item.title} </Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   );
 };
@@ -15,6 +41,18 @@ const Styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: "700",
     color: colors.black,
+  },
+  categoryBtn: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: colors.white,
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    borderRadius: 10,
+    shadowColor: "#333333",
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
   },
 });
 
---
+++ data/categories.ts
@@ -0,0 +1,44 @@
+const destinationCategories = [
+  {
+    title: "All",
+    iconName: "hiking",
+  },
+  {
+    title: "Beaches",
+    iconName: "beach",
+  },
+  {
+    title: "mountains",
+    iconName: "terrain",
+  },
+  {
+    title: "Cities",
+    iconName: "city",
+  },
+  {
+    title: "Forests",
+    iconName: "tree",
+  },
+  {
+    title: "Lakes",
+    iconName: "hiking",
+  },
+  {
+    title: "historical Sites",
+    iconName: "castle",
+  },
+  {
+    title: "Nationnal Parks",
+    iconName: "pine",
+  },
+  {
+    title: "Islands",
+    iconName: "hiking",
+  },
+  {
+    title: "Deserts",
+    iconName: "hiking",
+  },
+];
+
+export default destinationCategories;
