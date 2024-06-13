f8a65b6247cd2c76d8f146583f66fb4095960080
Ovoxoi <129445931+Ovoxoi@users.noreply.github.com>
2024-06-13 00:19:22 +0000

    .

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,15 +1,21 @@
 import { View, Text, FlatList } from "react-native";
 import React from "react";
 
-type Props={
-    listings: any[]
-}
+type Props = {
+  listings: any[];
+};
 
-const Listings = ({listings}:Props) => {
-    const renderItems =()
+const Listings = ({ listings }: Props) => {
+  const renderItems = ({ item }) => {
+    return (
+      <View>
+        <Text>{item.name}</Text>
+      </View>
+    );
+  };
   return (
     <View>
-       <FlatList data={listings} renderItem={renderItems}/>
+      <FlatList data={listings} renderItem={renderItems} />
     </View>
   );
 };
